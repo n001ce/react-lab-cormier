2c83bdbc82b124d394b9cb159739f772ae445b69
n001ce <a1ccormier@gmail.com>
2021-08-01 11:51:34 -0400

    1hr

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,20 +2,30 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = {
-        count: 0,
+        count: 1,
+    }
+
+    constructor(){
+        
+    }
+
+    handleIncrement(){
+        console.log("Increment Clicked", this)
+
     }
 
     render() { 
         return (
-        <div>
-            <span className="badge badge-primary">{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
-        </div>
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button
+                 onclick={this.handleIncrement} 
+                 className="btn btn-secondary btn-sm"
+                 >
+                     Increment
+                </button>
+            </div>
         )
-    }
-    formatCount(){
-        const {count} = this.state
-        return count===0 ? "Zero" : count
     }
 }
  
